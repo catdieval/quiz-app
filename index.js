98d8f389fd1fa2a81953d95539250be2631ea5d1
Catherine Dieval <catherine.dieval@hotmail.fr>
2023-11-27 10:12:23 +0100

    Add background color on click to bookmark button

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ const toggledarkBtn = document.querySelector('[data-js="toggledarkbutton"]');
 toggledarkBtn.addEventListener("click", () => {
   bodyElement.classList.toggle("darkmode");
 });
+
+const toggleBookmarkBtn = document.querySelector(
+  '[data-js="toggle-bookmark-button"]'
+);
+
+toggleBookmarkBtn.addEventListener("click", () => {
+  toggleBookmarkBtn.classList.toggle("selected-bookmark-button");
+});
