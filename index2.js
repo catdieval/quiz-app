9f132a4fcd8a81bfcfbfec98d43a3defd62a0300
Catherine <catherine.dieval@hotmail.fr>
2024-12-10 11:30:38 +0100

    Added appending of a new created card to section with class card-container in index2.js

--- index2.js
+++ index2.js
@@ -12,8 +12,29 @@ const toggleAnswerBtn = document.querySelector('[data-js="answer-button"]');
 const answerElement = document.querySelector('[data-js="answer"]');
 
 toggleAnswerBtn.addEventListener("click", () => {
-  
   toggleAnswerBtn.textContent = (answerElement.hasAttribute("hidden")) ? "Hide answer": "Show answer";
   answerElement.toggleAttribute("hidden"); 
   
 });
+
+if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
+    
+  const container = document.querySelector(".card-container");
+  let arrayNewQuestions = JSON.parse(localStorage.getItem("array-new-questions"));
+  arrayNewQuestions.map((obj) => {
+  const cardAdded = document.createElement("section");
+  cardAdded.classList.add("question-card-box");
+  cardAdded.innerHTML = `
+<p class="question">${obj.newQuestion}</p>
+        <button type="button" class="answer-button" data-js="answer-button">
+          Hide answer
+        </button>
+        <p class="question" data-js="answer">${obj.newAnswer}</p> 
+        <div class="tag-section">
+           <p>Tag</p>
+           <span class="category">${obj.newTag}</span>
+        </div>
+`;
+  container.appendChild(cardAdded);
+  });
+} 
