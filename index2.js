de75aa9623e2a6bce17ff2831360b33addf7cf5f
Catherine <catherine.dieval@hotmail.fr>
2024-12-10 14:52:20 +0100

    Added functionality for answer button toggling for all cards in index2.js

--- index2.js
+++ index2.js
@@ -8,15 +8,6 @@ toggleBookmarkBtn.addEventListener("click", () => {
   toggleBookmarkBtn.classList.toggle("selected-bookmark-button");
 });
 
-const toggleAnswerBtn = document.querySelector('[data-js="answer-button"]');
-const answerElement = document.querySelector('[data-js="answer"]');
-
-toggleAnswerBtn.addEventListener("click", () => {
-  toggleAnswerBtn.textContent = (answerElement.hasAttribute("hidden")) ? "Hide answer": "Show answer";
-  answerElement.toggleAttribute("hidden"); 
-  
-});
-
 if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
     
   const container = document.querySelector(".card-container");
@@ -37,4 +28,25 @@ if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
 `;
   container.appendChild(cardAdded);
   });
-} 
+
+  const listOfAnswerElements = document.querySelectorAll('[data-js="answer"]');
+  const listOfToggleAnswerBtns = document.querySelectorAll('[data-js="answer-button"]');
+
+  for (let i = 0; i < listOfToggleAnswerBtns.length; i++) {
+  
+    const btn = listOfToggleAnswerBtns[i];
+    const answer = listOfAnswerElements[i];
+    btn.addEventListener("click", () => {
+      btn.textContent = (answer.hasAttribute("hidden")) ? "Hide answer": "Show answer";
+      answer.toggleAttribute("hidden");
+    });
+  } 
+} else {
+  const toggleAnswerBtn = document.querySelector('[data-js="answer-button"]');
+  const answerElement = document.querySelector('[data-js="answer"]');
+
+  toggleAnswerBtn.addEventListener("click", () => {
+    toggleAnswerBtn.textContent = (answerElement.hasAttribute("hidden")) ? "Hide answer": "Show answer";
+    answerElement.toggleAttribute("hidden"); 
+  });
+}
