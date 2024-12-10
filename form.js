63173ea755e65cf8a86443b019518383582925ec
Catherine <catherine.dieval@hotmail.fr>
2024-12-10 11:26:23 +0100

    Made many changes to form.js

--- form.js
+++ form.js
@@ -7,32 +7,43 @@ const tagInput = document.querySelector('[data-js="tag-input"]');
 
 formElement.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const questionInputText = questionInput.value;
   const answerInputText = answerInput.value;
-  const tagInputText = tagInput.value;
-  console.log(questionInputText);
-  console.log(answerInputText);
-  console.log(tagInputText);
-  const newCard = document.createElement("section");
-  newCard.classList.add("question-card-box");
-  document.body.append(newCard);
+  const tagInputText = "#" + tagInput.value;
+
+  event.target.reset();
+
+  const newBreak = document.createElement("br");
+  document.body.append(newBreak);
   const newQuestion = document.createElement("p");
+  newQuestion.textContent = "New question: " + questionInputText;
   newQuestion.classList.add("question");
-  newQuestion.textContent = questionInputText;
-  newCard.append(newQuestion);
+  document.body.append(newQuestion);
   const newAnswer = document.createElement("p");
+  newAnswer.textContent = "New answer: " + answerInputText;
   newAnswer.classList.add("question");
-  newAnswer.textContent = answerInputText;
-  newCard.append(newAnswer);
-  const newUl = document.createElement("ul");
-  newCard.append(newUl);
-  const newLi = document.createElement("li");
-  newLi.classList.add("button-category");
-  newUl.append(newLi);
-  const newTag = document.createElement("button");
-  newTag.classList.add("colored-button");
-  newTag.textContent = tagInputText;
-  newLi.append(newTag);
+  document.body.append(newAnswer);
+  const newTag = document.createElement("p");
+  newTag.textContent = "New tag: " + tagInputText;
+  newTag.classList.add("question");
+  document.body.append(newTag);
+  
+  const obj = {
+    newQuestion: questionInputText, newAnswer: answerInputText, newTag: tagInputText
+  };
+  
+  let arrayNewQuestions;
+  if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
+    arrayNewQuestions = JSON.parse(localStorage.getItem("array-new-questions"));
+    arrayNewQuestions.push(obj);
+  } else {
+    arrayNewQuestions = [obj];
+  }
+ 
+  
+  
+  localStorage.setItem("array-new-questions", JSON.stringify(arrayNewQuestions)); 
 });
 
 function calculateAmountLeft(jsAmountLeft, textAreaInput) {
