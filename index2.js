bc0879ec5a3331f7b97b02a499b3e5d420459a13
Catherine <catherine.dieval@hotmail.fr>
2024-12-11 11:39:35 +0100

    Added functionality for toggle bookmark button for the present card

--- index2.js
+++ index2.js
@@ -1,4 +1,9 @@
-console.clear();
+//console.clear();
+/*localStorage.removeItem("array-bookmarks");
+localStorage.removeItem("array-new-questions");
+localStorage.removeItem("array-is-bookmarked"); */
+
+
 
 if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
     
@@ -8,14 +13,14 @@ if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
   const cardAdded = document.createElement("section");
   cardAdded.classList.add("question-card-box");
   cardAdded.innerHTML = `
-<p class="question">${obj.newQuestion}</p>
+<p class="question" data-js="question">${obj.newQuestion}</p>
         <button type="button" class="answer-button" data-js="answer-button">
           Hide answer
         </button>
         <p class="question" data-js="answer">${obj.newAnswer}</p> 
         <div class="tag-section">
            <p>Tag</p>
-           <span class="category">${obj.newTag}</span>
+           <span class="category" data-js="tag">${obj.newTag}</span>
         </div>
         <button
           type="button"
@@ -31,38 +36,114 @@ if (JSON.parse(localStorage.getItem("array-new-questions")) !== null) {
   });
 
   const listOfToggleBookmarkBtns = document.querySelectorAll('[data-js="toggle-bookmark-button"]');
-
+  const listOfQuestions = document.querySelectorAll('[data-js="question"]');
+  const listOfAnswers = document.querySelectorAll('[data-js="answer"]');
+  const listOfTags = document.querySelectorAll('[data-js="tag"]');
+  
   for (let i = 0; i < listOfToggleBookmarkBtns.length; i++) {
     const btn = listOfToggleBookmarkBtns[i];
+    const questionElement = listOfQuestions[i];
+    const answerElement = listOfAnswers[i];
+    const tagElement = listOfTags[i];
+
     btn.addEventListener("click", () => {
       btn.classList.toggle("selected-bookmark-button");
+      btn.toggleAttribute("isBookmarked");
+      
+      if (btn.hasAttribute("isBookmarked")) {
+
+        const answer = answerElement.textContent;
+        const question = questionElement.textContent;
+        const tag = tagElement.textContent;
+
+        const obj = {
+          bookmarkedQuestion: question, bookmarkedAnswer: answer, bookmarkedTag: tag
+        };
+
+        let arrayBookmarks;
+        if (JSON.parse(localStorage.getItem("array-bookmarks")) !== null) {
+          arrayBookmarks = JSON.parse(localStorage.getItem("array-bookmarks"));
+          arrayBookmarks.push(obj);
+        } else {
+          arrayBookmarks = [obj];
+        } 
+  
+        localStorage.setItem("array-bookmarks", JSON.stringify(arrayBookmarks)); 
+      }
+      
     });
   }
 
-  const listOfAnswerElements = document.querySelectorAll('[data-js="answer"]');
   const listOfToggleAnswerBtns = document.querySelectorAll('[data-js="answer-button"]');
 
   for (let i = 0; i < listOfToggleAnswerBtns.length; i++) {
   
     const btn = listOfToggleAnswerBtns[i];
-    const answer = listOfAnswerElements[i];
+    const answer = listOfAnswers[i];
     btn.addEventListener("click", () => {
       btn.textContent = (answer.hasAttribute("hidden")) ? "Hide answer": "Show answer";
       answer.toggleAttribute("hidden");
     });
   } 
+
 } else {
 
   const toggleBookmarkBtn = document.querySelector(
     '[data-js="toggle-bookmark-button"]'
   );
-  
+  const answerElement = document.querySelector('[data-js="answer"]');
+  const questionElement = document.querySelector('[data-js="question"]');
+  const tagElement = document.querySelector('[data-js="tag"]');
+ 
+  let arrayIsBookmarked;
+  if (JSON.parse(localStorage.getItem("array-is-bookmarked")) !== null) {
+    arrayIsBookmarked = JSON.parse(localStorage.getItem("array-is-bookmarked"));
+
+    if (arrayIsBookmarked[0] === true) {
+      toggleBookmarkBtn.classList.add("selected-bookmark-button");
+      toggleBookmarkBtn.setAttribute("isBookmarked", "");
+    } else {
+      toggleBookmarkBtn.classList.remove("selected-bookmark-button");
+      toggleBookmarkBtn.removeAttribute("isBookmarked");
+    }
+    
+  } else {
+    arrayIsBookmarked = [false];
+    localStorage.setItem("array-is-bookmarked", JSON.stringify(arrayIsBookmarked));
+  }
+
   toggleBookmarkBtn.addEventListener("click", () => {
     toggleBookmarkBtn.classList.toggle("selected-bookmark-button");
+    toggleBookmarkBtn.toggleAttribute("isBookmarked");
+
+    if (toggleBookmarkBtn.hasAttribute("isBookmarked")) {
+      arrayIsBookmarked = [true];
+      localStorage.setItem("array-is-bookmarked", JSON.stringify(arrayIsBookmarked));
+      console.log("Ho");
+
+        if (JSON.parse(localStorage.getItem("array-bookmarks")) === null) {
+            console.log("boo");
+            const answer = answerElement.textContent;
+            const question = questionElement.textContent;
+            const tag = tagElement.textContent;
+
+            const obj = {
+             bookmarkedQuestion: question, bookmarkedAnswer: answer, bookmarkedTag: tag
+            };
+
+            let arrayBookmarks = [obj];
+        
+            localStorage.setItem("array-bookmarks", JSON.stringify(arrayBookmarks)); 
+         }
+    }
+    else {
+        arrayIsBookmarked = [false];
+        console.log("Hi")
+        localStorage.setItem("array-is-bookmarked", JSON.stringify(arrayIsBookmarked));
+    }  
   });
 
   const toggleAnswerBtn = document.querySelector('[data-js="answer-button"]');
-  const answerElement = document.querySelector('[data-js="answer"]');
 
   toggleAnswerBtn.addEventListener("click", () => {
     toggleAnswerBtn.textContent = (answerElement.hasAttribute("hidden")) ? "Hide answer": "Show answer";
