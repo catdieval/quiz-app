70b157d87a44632e81eb171d97768a41e09adecf
Catherine <catherine.dieval@hotmail.fr>
2024-12-11 18:07:19 +0100

    Renamed index.js to profile.js

---
+++ profile.js
@@ -0,0 +1,8 @@
+//console.clear();
+
+const bodyElement = document.querySelector('[data-js="body"]');
+const toggledarkBtn = document.querySelector('[data-js="toggledarkbutton"]');
+
+toggledarkBtn.addEventListener("click", () => {
+  bodyElement.classList.toggle("darkmode");
+});
