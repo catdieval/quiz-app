cabfb8481dcba41f312a2f4dc89ccdc99f4b3a83
Catherine <catherine.dieval@hotmail.fr>
2024-12-11 18:29:38 +0100

    Improved look of profile page in dark mode in profile.js

--- profile.js
+++ profile.js
@@ -2,7 +2,14 @@
 
 const bodyElement = document.querySelector('[data-js="body"]');
 const toggledarkBtn = document.querySelector('[data-js="toggledarkbutton"]');
+const h1Element = document.querySelector('[data-js="h1"]');
+const titleBoxElement= document.querySelector('[data-js="titlebox"]');
+const hrElement = document.querySelector('[data-js="hr"]');
 
 toggledarkBtn.addEventListener("click", () => {
+
   bodyElement.classList.toggle("darkmode");
+  h1Element.classList.toggle("darkmode");
+  titleBoxElement.classList.toggle("darkmode");
+  hrElement.classList.toggle("darkmode");
 });
