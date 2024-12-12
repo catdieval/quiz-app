6dfdf47a8f2ab23439fb10964d72386a9f557216
Catherine <catherine.dieval@hotmail.fr>
2024-12-12 15:05:08 +0100

    Made many changes to profile.js

--- profile.js
+++ profile.js
@@ -6,10 +6,45 @@ const h1Element = document.querySelector('[data-js="h1"]');
 const titleBoxElement= document.querySelector('[data-js="titlebox"]');
 const hrElement = document.querySelector('[data-js="hr"]');
 
-toggledarkBtn.addEventListener("click", () => {
+let theme;
+  if (localStorage.getItem("darkLightTheme") === null) {
+    theme = "lightMode";
+    localStorage.setItem("darkLightTheme", theme);
+  } else {
+    theme = localStorage.getItem("darkLightTheme");
+  }
 
-  bodyElement.classList.toggle("darkmode");
-  h1Element.classList.toggle("darkmode");
-  titleBoxElement.classList.toggle("darkmode");
-  hrElement.classList.toggle("darkmode");
+  if (theme === "darkMode") {
+    bodyElement.classList.add("darkmode");
+    h1Element.classList.add("darkmode");
+    titleBoxElement.classList.add("darkmode");
+    hrElement.classList.add("darkmode");
+    
+  } else if (theme === "lightMode") {
+    bodyElement.classList.remove("darkmode");
+    h1Element.classList.remove("darkmode");
+    titleBoxElement.classList.remove("darkmode");
+    hrElement.classList.remove("darkmode");
+  }
+
+toggledarkBtn.addEventListener("click", () => {
+  bodyElement.toggleAttribute("isDark");
+
+  if (bodyElement.hasAttribute("isDark")) {
+    theme = "darkMode";
+    localStorage.setItem("darkLightTheme", theme);
+
+    bodyElement.classList.add("darkmode");
+    h1Element.classList.add("darkmode");
+    titleBoxElement.classList.add("darkmode");
+    hrElement.classList.add("darkmode");
+  } else {
+    theme = "lightMode";
+    localStorage.setItem("darkLightTheme", theme);
+
+    bodyElement.classList.remove("darkmode");
+    h1Element.classList.remove("darkmode");
+    titleBoxElement.classList.remove("darkmode");
+    hrElement.classList.remove("darkmode");
+  }
 });
